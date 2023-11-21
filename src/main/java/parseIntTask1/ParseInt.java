a62bbc7a8f89e43ddc855d7631e271d08ec14241
Abdurakhmon Bainazarov <baynazarowv@gmail.com>
2023-11-21 17:07:49 +0400

    Initial commit

--- src/main/java/parseIntTask1/ParseInt.java
+++ src/main/java/parseIntTask1/ParseInt.java
@@ -1,4 +1,41 @@
 package parseIntTask1;
 
+import java.util.Objects;
+
 public class ParseInt {
+
+    public static void main(String[] args) {
+        String s = "123";
+        int number = parseInt(s);
+        System.out.println(number);
+    }
+
+    public static int parseInt(String s) {
+
+        int number = 0;
+        boolean isNegative = false;
+
+        if(Objects.equals(s, ""))
+            throw new StringIndexOutOfBoundsException("Строка не может быть пустым: " + s);
+
+        if(s.charAt(0) == '-') {
+            isNegative = true;
+            s = s.substring(1); // Подменяем первый индекс на 1
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(!Character.isDigit(c)) // проверка является ли значение цифрой
+                throw new NumberFormatException("Неправильный формат значения: " + s);
+            int digit = s.charAt(i) - '0'; // Таким образом приводим к числовому значению
+            number = number * 10 + digit; // Освобождаем место для нового значения
+            if (number < Integer.MIN_VALUE/10 && digit > 7 || number > Integer.MAX_VALUE)
+                throw  new IndexOutOfBoundsException("Значение вышло за допустимый диапазон: " + s);
+        }
+
+        if (isNegative)
+            number = -number;
+
+        return number;
+    }
 }
--- src/test/java/parseIntTask1Test/ParseIntTest.java
+++ src/test/java/parseIntTask1Test/ParseIntTest.java
@@ -1,4 +1,55 @@
 package parseIntTask1Test;
 
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import static org.junit.Assert.assertEquals;
+import static parseIntTask1.ParseInt.parseInt;
+
 public class ParseIntTest {
+
+    @Test
+    public void positiveTest() {
+        String s = "123";
+        int test = parseInt(s);
+        assertEquals(123, test);
+    }
+
+    @Test
+    public void negativeTest() {
+        String s = "-123";
+        int test = parseInt(s);
+        assertEquals(-123, test);
+    }
+
+    @Test
+    public void stringIndexOutOfBoundsExceptionTest() {
+        String s = "";
+        IndexOutOfBoundsException thrown = Assertions.assertThrows(StringIndexOutOfBoundsException.class, () -> {
+            parseInt(s);
+        }, "StringIndexOutOfBoundsException error was expected");
+
+        Assertions.assertEquals("Строка не может быть пустым: " + s, thrown.getMessage());
+    }
+
+    @Test
+    public void indexOfBoundExceptionTest() {
+        String s = "99999999999999999";
+        IndexOutOfBoundsException thrown = Assertions.assertThrows(IndexOutOfBoundsException.class, () -> {
+            parseInt(s);
+        }, "IndexOutOfBoundException error was expected");
+
+        Assertions.assertEquals("Значение вышло за допустимый диапазон: " + s, thrown.getMessage());
+    }
+
+    @Test
+    public void numberFormatterExceptionTest() {
+        String s = "One";
+        NumberFormatException thrown = Assertions
+                .assertThrows(NumberFormatException.class, () -> {
+                    parseInt(s);
+                }, "NumberFormatException error was expected");
+
+        Assertions.assertEquals("Неправильный формат значения: " + s, thrown.getMessage());
+    }
 }
